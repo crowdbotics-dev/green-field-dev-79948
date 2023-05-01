7de46d28512c13e12145892e22d359cc91040ebc
Crowdbotics <engineering@crowdbotics.com>
2023-05-01 23:09:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-field-dev-79948",
+  url: "https://green-field-dev-79948.botics.co",
+  api: "https://green-field-dev-79948.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
